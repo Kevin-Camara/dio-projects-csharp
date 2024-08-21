798dd840d453581d89b13f4649d66f52155359a4
kevin-Camara <kevinkrc2003@gmail.com>
2024-08-21 00:02:32 -0300

    Projeto Smartphone

---
+++ DesafioPOO_/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "Modelo 1", "1111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iphone: ");
+Smartphone iphone = new Iphone("4987", "Modelo 2", "2222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
